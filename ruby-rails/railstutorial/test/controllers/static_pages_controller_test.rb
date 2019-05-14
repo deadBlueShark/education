2ca12923fc59457efbc03713d2b5be1f49a55004
chiNguyenTrex <xspacechinguyen@gmail.com>
2019-05-14 15:02:15 +0700

    global-search

--- ruby-rails/railstutorial/test/controllers/static_pages_controller_test.rb
+++ ruby-rails/railstutorial/test/controllers/static_pages_controller_test.rb
@@ -10,25 +10,25 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should get home page' do
-    get static_pages_home_url
+    get root_url
     assert_response :success
     assert_select 'title', @@base_title
   end
 
   test 'should get help page' do
-    get static_pages_help_url
+    get help_url
     assert_response :success
     assert_select 'title', "Help | #{@@base_title}"
   end
 
   test 'should get about page' do
-    get static_pages_about_url
+    get about_url
     assert_response :success
     assert_select 'title', "About | #{@@base_title}"
   end
 
   test 'shoud get contact page' do
-    get static_pages_contact_url
+    get contact_url
     assert_response :success
     assert_select 'title', "Contact | #{@@base_title}"
   end
