783f741029e3206738fe467819aaba64af8fc69b
chiNguyenTrex <xspacechinguyen@gmail.com>
2019-05-30 15:52:20 +0700

    token-base-email-authen

---
+++ ruby-rails/devise-token-auth/db/seeds.rb
@@ -0,0 +1,8 @@
+User.create(
+  email: 'token-base-auth@example.com',
+  nickname: 'Winter Soldier',
+  name: 'Bucky Barnes',
+  password: '123456'
+)
+
+SuperHero.create(name: 'Captain America', skill: 'Super Soldier')
