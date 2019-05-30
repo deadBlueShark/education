5ffb1df077be599665fedccd1b62abd2a0a5c23d
chiNguyenTrex <xspacechinguyen@gmail.com>
2019-05-30 13:38:40 +0700

    confg

--- ruby-rails/railstutorial/config/application.rb
+++ ruby-rails/railstutorial/config/application.rb
@@ -1,17 +1,18 @@
 require_relative 'boot'
 
-require "rails"
+require 'rails'
+
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-# require "action_cable/engine"
-require "sprockets/railtie"
-require "rails/test_unit/railtie"
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+# require 'action_cable/engine'
+require 'sprockets/railtie'
+require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -23,8 +24,8 @@ module Railstutorial
 
     config.generators do |generator|
       generator.assets false
-      #generator.test_framework nil
-      #generator.test_framework :minitest, spec: false, fixture: false
+      generator.test_framework nil
+      generator.test_framework :minitest, spec: false, fixture: false
     end
   end
 end
