06ee880e64526cb69f71f0bdc834022a3de0c2c8
NguyenLC <xspacechinguyen@gmail.com>
2021-11-28 22:38:36 +0700

    React component

--- react/hello-react/src/App.js
+++ react/hello-react/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Person from './components/Person';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Hello React</h2>
+      <Person name="Nguyen" age="28"></Person>
+      <Person name="David" age="30">Hobbies: Learning</Person>
     </div>
   );
 }
+
+// Not use JSX
+// function App() {
+//   return (
+//     React.createElement("div", {className: "App"},
+//       React.createElement("div", null, "Hello ReactJS"))
+//   )
+// }
 
 export default App;
---
+++ react/hello-react/src/components/Person.js
@@ -0,0 +1,21 @@
+const Person = (props) => {
+  return (
+    <div>
+      <p>I'm {props.name}, {props.age} years old.</p>
+      <i>{props.children}</i>
+    </div>
+  )
+}
+
+// Old styles:
+//import { Component } from 'react';
+// class Person extends Component {
+//   render() {
+//     return (<p>I'm {this.props.name}. {this.props.age} years sold.</p>)
+//   }
+// }
+
+export default Person;
+
+
+
