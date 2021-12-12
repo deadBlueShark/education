f90cd6af0039fa77af4e6704da863252497a9d65
NguyenLC <xspacechinguyen@gmail.com>
2021-12-12 19:40:45 +0700

    List products and sort

--- react/voting-app/src/components/Product.js
+++ react/voting-app/src/components/Product.js
@@ -1,10 +1,32 @@
 import React from 'react'
 
 class Product extends React.Component {
+  // Component can’t modify its props. props is immutable.
+
+
   render() {
+    const product = this.props.product
+
     return (
       <div className="item">
-        <div className="image"></div>
+        <div className="image">
+          <img src={product.productImageUrl} />
+        </div>
+
+        <div className="middle aligned content">
+          <div>
+            <a><i className="large caret up icon" /></a>
+            {product.votes}
+          </div>
+          <div className="description">
+            <a href={product.url}>{product.title}</a>
+            <p>{product.description}</p>
+          </div>
+          <div className="extra">
+            <span>Submitted by:</span>
+            <img className="ui avatar image" src={product.productImageUrl} />
+          </div>
+        </div>
       </div>
     )
   }
--- react/voting-app/src/components/ProductList.js
+++ react/voting-app/src/components/ProductList.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Product from './Product'
+import Products from '../data/seed'
 
 class ProductList extends React.Component {
   render() {
+    const products = Products.sort((a, b) => a.votes - b.votes)
+    const productComponents = products.map((product) => {
+      return <Product key={`product-${product.id}`} product={product} />
+    })
+
     return (
       <div className="ui unstackable items">
-        Hello, friends! I am a basic React Component
+        {productComponents}
       </div>
     )
   }
