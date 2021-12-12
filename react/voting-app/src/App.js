7411463f1db824dd72d142938ce29af7f4f97f2e
NguyenLC <xspacechinguyen@gmail.com>
2021-12-12 15:26:02 +0700

    Start voting app from Fullstack React Book

---
+++ react/voting-app/src/App.js
@@ -0,0 +1,13 @@
+import 'semantic-ui-css/semantic.min.css'
+import './App.css';
+import ProductList from './components/ProductList'
+
+function App() {
+  return (
+    <ProductList />
+  )
+}
+
+export default App;
+
+
---
+++ react/voting-app/src/components/ProductList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ProductList extends React.Component {
+  render() {
+    return (
+      <div className="ui unstackable items">
+        Hello, friends! I am a basic React Component
+      </div>
+    )
+  }
+}
+
+export default ProductList
