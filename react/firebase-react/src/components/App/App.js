b4b86eb99405b20f66e362b10d31dab5fe8a50e9
NguyenLC <xspacechinguyen@gmail.com>
2021-12-23 19:26:49 +0700

    Session state handle

--- react/firebase-react/src/components/App/App.js
+++ react/firebase-react/src/components/App/App.js
@@ -9,25 +9,50 @@ import PasswordForgetPage from '../PasswordForget';
 import HomePage from '../Home';
 import AccountPage from '../Account';
 import AdminPage from '../Admin';
+import {withFirebase} from '../Firebase'
 
 import * as ROUTES from '../../constants/routes';
 
-const App = () => (
-  <BrowserRouter>
-    <div>
-      <Navigation />
+class App extends React.Component {
+  constructor(props) {
+    super(props)
 
-      <hr />
+    this.state = {
+      authUser: null
+    }
+  }
 
-        <Route exact path={ROUTES.LANDING} component={LandingPage}/>
-        <Route exact path={ROUTES.SIGN_UP} component={SignUpPage}/>
-        <Route exact path={ROUTES.SIGN_IN} component={SignInPage}/>
-        <Route exact path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage}/>
-        <Route exact path={ROUTES.HOME} component={HomePage}/>
-        <Route exact path={ROUTES.ACCOUNT} component={AccountPage}/>
-        <Route exact path={ROUTES.ADMIN} component={AdminPage}/>
-    </div>
-  </BrowserRouter>
-);
+  componentDidMount() {
+    this.unsubscribe = this.props.firebase.auth.onAuthStateChanged(authUser => {
+      authUser ? this.setState({authUser}) : this.setState({authUser: null})
+    })
+  }
 
-export default App;
+  // We also want to avoid memory leaks that lead to performance issues,
+  // so we’ll remove the listener if the component unmounts.
+  componentWillUnmount() {
+    this.unsubscribe()
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Navigation authUser={this.state.authUser} />
+
+          <hr />
+
+          <Route exact path={ROUTES.LANDING} component={LandingPage}/>
+          <Route exact path={ROUTES.SIGN_UP} component={SignUpPage}/>
+          <Route exact path={ROUTES.SIGN_IN} component={SignInPage}/>
+          <Route exact path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage}/>
+          <Route exact path={ROUTES.HOME} component={HomePage}/>
+          <Route exact path={ROUTES.ACCOUNT} component={AccountPage}/>
+          <Route exact path={ROUTES.ADMIN} component={AdminPage}/>
+        </div>
+      </BrowserRouter>
+    )
+  }
+}
+
+export default withFirebase(App);
--- react/firebase-react/src/components/Navigation/index.js
+++ react/firebase-react/src/components/Navigation/index.js
@@ -4,17 +4,31 @@ import SignOut from '../SignOut'
 
 import * as ROUTES from '../../constants/routes'
 
-const Navigation = () => {
+const Navigation = ({authUser}) => {
+  return <div>{authUser ? <NavigationAuth authUser={authUser} /> : <NavigationNonAuth />}</div>
+}
+
+const NavigationAuth = ({authUser}) => {
+  return (
+    <div>
+      <label>{authUser.email}</label>
+      <ul>
+        <li><Link to={ROUTES.LANDING}>Landing</Link></li>
+        <li><Link to={ROUTES.HOME}>Home</Link></li>
+        <li><Link to={ROUTES.ACCOUNT}>Account</Link></li>
+        <li><SignOut /></li>
+      </ul>
+    </div>
+  )
+}
+
+const NavigationNonAuth = () => {
   return (
     <div>
       <ul>
         <li><Link to={ROUTES.SIGN_IN}>Signin</Link></li>
         <li><Link to={ROUTES.SIGN_UP}>Sign Up</Link></li>
         <li><Link to={ROUTES.LANDING}>Landing</Link></li>
-        <li><Link to={ROUTES.HOME}>Home</Link></li>
-        <li><Link to={ROUTES.ACCOUNT}>Account</Link></li>
-        <li><Link to={ROUTES.ADMIN}>Admin</Link></li>
-        <li><SignOut /></li>
       </ul>
     </div>
   )
