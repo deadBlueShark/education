9546445789957d16127fdf286e199ea04aeca04d
Chi Nguyen Le <xspacechinguyen@gmail.com>
2022-01-02 12:54:33 +0700

    Refactor move initial state to reducer

--- react/skypey-redux/src/reducers/index.js
+++ react/skypey-redux/src/reducers/index.js
@@ -1,3 +1,9 @@
-export default (state, action) => {
+import { contacts } from '../static-data'
+
+export const contactReducer = (state = { contacts }, action) => {
+  return state
+}
+
+export const messageReducer = (state = {}, action) => {
   return state
 }
--- react/skypey-redux/src/store/index.js
+++ react/skypey-redux/src/store/index.js
@@ -1,8 +1,7 @@
 import { createStore } from 'redux'
 
-import reducer from '../reducers'
-import { contacts } from '../static-data'
+import { contactReducer } from '../reducers'
 
-const store = createStore(reducer, { contacts })
+const store = createStore(contactReducer)
 
 export default store
