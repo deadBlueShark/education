1b9f0b35a7871e1f29cf86e228ae69b9df4aa56b
Chi Nguyen Le <xspacechinguyen@gmail.com>
2022-01-14 20:20:19 +0700

    class-based component, monster list

--- react/monster-rolodex/src/App.js
+++ react/monster-rolodex/src/App.js
@@ -1,12 +1,36 @@
 import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+  constructor() {
+    super();
 
-    </div>
-  );
+    this.state = {
+      monsters: [
+        {
+          id: 1,
+          name: "Frankenstein"
+        },
+        {
+          id: 2,
+          name: "Dracula"
+        },
+        {
+          id: 3,
+          name: "Zoombie"
+        }
+      ]
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        {
+          this.state.monsters.map(monster =>
+            <li key={monster.id}>{monster.name}</li>)
+        }
+      </div>
+    );
+  }
 }
-
 export default App;
