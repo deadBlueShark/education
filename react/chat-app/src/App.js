57f227ca45ecb2e74a46e87c79d6a8ddeaa0d07e
Chi Nguyen Le <xspacechinguyen@gmail.com>
2022-01-27 16:31:09 +0700

    Make some layout

--- react/chat-app/src/App.js
+++ react/chat-app/src/App.js
@@ -1,9 +1,17 @@
-function App() {
-  return (
-    <div className="App">
+import React from "react";
 
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div id="container">
+        <aside id="sidebar">Users</aside>
+        <section id="main">
+          <section id="messages-list">Message List</section>
+          <section id="new-message">New Message</section>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
