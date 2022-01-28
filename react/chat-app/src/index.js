8cfcbb66cadfd61ad95ccfeaacd4e8e106b738f2
Chi Nguyen Le <xspacechinguyen@gmail.com>
2022-01-28 11:56:19 +0700

    Add Provider, create Store on top level application

--- react/chat-app/src/index.js
+++ react/chat-app/src/index.js
@@ -4,9 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+import chatReducer from "./reducers";
+
+const chatStore = createStore(chatReducer);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={chatStore}><App /></Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
