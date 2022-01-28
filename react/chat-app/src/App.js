f58e78be8382ea6b4452eaded6da9baa0ed1ec31
Chi Nguyen Le <xspacechinguyen@gmail.com>
2022-01-28 16:48:47 +0700

    Add container components to manage behaviour

--- react/chat-app/src/App.js
+++ react/chat-app/src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 
+import { Sidebar } from "./containers/Sidebar";
+import { MessagesList } from "./containers/MessagesList";
+import { AddMessage } from "./containers/AddMessage";
+
 class App extends React.Component {
   render() {
     return (
       <div id="container">
-        <aside id="sidebar">Users</aside>
+        <Sidebar />
         <section id="main">
-          <section id="messages-list">Message List</section>
-          <section id="new-message">New Message</section>
+          <MessagesList />
+          <AddMessage />
         </section>
       </div>
     );
