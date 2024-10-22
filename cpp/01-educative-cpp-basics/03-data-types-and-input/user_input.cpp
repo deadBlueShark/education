cb3ddeba0a790a103fa35b85a386f65dbb904295
deadBlueShark <xspacechinguyen@gmail.com>
2024-10-23 01:39:38 +0700

    Take user input(from keyboard) use 'cin'

---
+++ cpp/01-educative-cpp-basics/03-data-types-and-input/user_input.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  unsigned short int age;
+  cout << "What is your age?" << endl;
+  cin >> age;
+  cout << "You are " << age << " years old" << endl;
+
+  string name;
+  cout << "What is your name?" << endl;
+  cin >> name;
+  cout << "Your name is " << name << endl;
+}
