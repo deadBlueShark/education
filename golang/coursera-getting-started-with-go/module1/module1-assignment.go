3869c95ec0723475b22e6459770c06d229777bc2
deadBlueShark <xspacechinguyen@gmail.com>
2025-01-27 14:29:50 +0700

    Reformat Go source code

--- golang/coursera-getting-started-with-go/module1/module1-assignment.go
+++ golang/coursera-getting-started-with-go/module1/module1-assignment.go
@@ -4,9 +4,11 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, world!")
+
 	x := "Nguyen"
 	fmt.Printf("Hi " + x + "\n")
 	fmt.Printf("Hi %s\n", x)
+
 	fmt.Printf("Hello World 2\n")
 	fmt.Printf("Hi %s 2.\n", x)
 }
