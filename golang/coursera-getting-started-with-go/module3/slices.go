d2f52cd02640412b4b2f633fed4652c534676325
deadBlueShark <xspacechinguyen@gmail.com>
2025-01-28 09:39:58 +0700

    Refactor code

--- golang/coursera-getting-started-with-go/module3/slices.go
+++ golang/coursera-getting-started-with-go/module3/slices.go
@@ -4,16 +4,22 @@ import "fmt"
 
 func main() {
 	arr := [...]int{1, 2, 3, 4, 4, 5, 4, 2, 3, 2}
-	slice1 := arr[1:3] // [2 3]
-	slice2 := arr[2:5] // [3 4 4]
+	slice1 := arr[1:3]
+	fmt.Println("slice1: ", slice1) // [2 3]
+
+	slice2 := arr[2:5]
+	fmt.Println("slice2: ", slice2) // [3 4 4]
+
 	slice3 := arr[0:10]
+	fmt.Println("slice3: ", slice3)
 
 	// slice literal
 	slice4 := []int{1, 2, 3, 4}
 
 	// using make
 	slice5 := make([]int, 5, 10)
-	fmt.Printf("Length: %v Capacity: %v\n", len(slice1), cap(slice1)) // Lengsth: 2 Capacity: 9
+
+	fmt.Printf("Length: %v Capacity: %v\n", len(slice1), cap(slice1)) // Length: 2 Capacity: 9
 	fmt.Printf("Length: %v Capacity: %v\n", len(slice2), cap(slice2)) // Length: 3 Capacity: 8
 	fmt.Printf("Length: %v Capacity: %v\n", len(slice3), cap(slice3)) // Length: 10 Capacity: 10
 	fmt.Printf("Length: %v Capacity: %v\n", len(slice4), cap(slice4)) // Length: 4 Capacity: 4
