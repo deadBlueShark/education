a944ec286caafee6bf4ce8ac2f1dd897c0beb9ba
deadBlueShark <xspacechinguyen@gmail.com>
2025-02-09 10:35:59 +0700

    Go binary search with recursion

--- golang/book-data-structures-algorithms-in-go/01-intro/practice/binary-search.go
+++ golang/book-data-structures-algorithms-in-go/01-intro/practice/binary-search.go
@@ -20,9 +20,27 @@ func binarySearchV1(arrPtr *[]int, target int) bool {
 	return false
 }
 
+func binarySearchV2(arrPtr *[]int, target, startIdx, endIdx int) bool {
+	if startIdx > endIdx {
+		return false
+	}
+
+	midIdx := (startIdx + endIdx) / 2
+
+	if (*arrPtr)[midIdx] == target {
+		return true
+	} else if (*arrPtr)[midIdx] > target {
+		return binarySearchV2(arrPtr, target, startIdx, midIdx-1)
+	} else {
+		return binarySearchV2(arrPtr, target, midIdx+1, endIdx)
+	}
+}
+
 func main() {
 	// Array must be sorted(this example is ASC)
 	arr := []int{1, 3, 5, 7, 9}
+
+	fmt.Println("BINARY SEARCH V1:")
 	fmt.Printf("Is 9 existed in arr? %t\n", binarySearchV1(&arr, 9))
 	fmt.Printf("Is 7 existed in arr? %t\n", binarySearchV1(&arr, 7))
 	fmt.Printf("Is 5 existed in arr? %t\n", binarySearchV1(&arr, 5))
@@ -32,4 +50,16 @@ func main() {
 	fmt.Printf("Is -2 existed in arr? %t\n", binarySearchV1(&arr, -2))
 	fmt.Printf("Is 10 existed in arr? %t\n", binarySearchV1(&arr, 10))
 	fmt.Printf("Is 1000 existed in arr? %t\n", binarySearchV1(&arr, 1000))
+
+	fmt.Println("BINARY SEARCH V2:")
+	arrLastIdx := len(arr) - 1
+	fmt.Printf("Is 9 existed in arr? %t\n", binarySearchV2(&arr, 9, 0, arrLastIdx))
+	fmt.Printf("Is 7 existed in arr? %t\n", binarySearchV2(&arr, 7, 0, arrLastIdx))
+	fmt.Printf("Is 5 existed in arr? %t\n", binarySearchV2(&arr, 5, 0, arrLastIdx))
+	fmt.Printf("Is 3 existed in arr? %t\n", binarySearchV2(&arr, 3, 0, arrLastIdx))
+	fmt.Printf("Is 1 existed in arr? %t\n", binarySearchV2(&arr, 1, 0, arrLastIdx))
+	fmt.Printf("Is 0 existed in arr? %t\n", binarySearchV2(&arr, 0, 0, arrLastIdx))
+	fmt.Printf("Is -2 existed in arr? %t\n", binarySearchV2(&arr, -2, 0, arrLastIdx))
+	fmt.Printf("Is 10 existed in arr? %t\n", binarySearchV2(&arr, 10, 0, arrLastIdx))
+	fmt.Printf("Is 1000 existed in arr? %t\n", binarySearchV2(&arr, 1000, 0, arrLastIdx))
 }
