e4bc5d8e2cfd9448e0c2bc55a5618bdc02c0a24c
deadBlueShark <xspacechinguyen@gmail.com>
2025-03-01 00:20:01 +0700

    Variable type

---
+++ python/bootdev-learn-to-code-in-python/variables/variable_type.py
@@ -0,0 +1,7 @@
+name = "John"
+age = 30
+colors = ["red", "blue"]
+
+print(f"Type of name: {type(name).__name__}")  # str
+print(f"Type of age: {type(age).__name__}")  # int
+print(f"Type of colors: {type(colors).__name__}")  # list
