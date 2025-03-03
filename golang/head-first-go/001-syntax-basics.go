0d87682975c8fbee78aae4f7bf658c8d71a7fde6
deadBlueShark <xspacechinguyen@gmail.com>
2025-03-03 23:02:44 +0700

    Fix string deprecated method

--- golang/head-first-go/001-syntax-basics.go
+++ golang/head-first-go/001-syntax-basics.go
@@ -4,14 +4,17 @@ import (
 	"fmt"
 	"math"
 	"reflect"
-	"strings"
+
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
 )
 
 func main() {
 	fmt.Println("Hello, Go!")
 	fmt.Println("Hello again,", "Nguyen")
 	fmt.Println(math.Floor(2.75))
-	fmt.Println(strings.Title("le chi nguyen"))
+	// Deprecated: strings.Title("le chi nguyen")
+	fmt.Println(cases.Title(language.English).String("le chi nguyen"))
 	fmt.Println("Division:", 9/5, 9%5)
 
 	// Runes
