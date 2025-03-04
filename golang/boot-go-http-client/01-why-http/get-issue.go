436f8bb8a8c262dc73259272fb6f20d6298e993b
deadBlueShark <xspacechinguyen@gmail.com>
2025-03-04 18:47:53 +0700

    Refactor code

--- golang/boot-go-http-client/01-why-http/get-issue.go
+++ golang/boot-go-http-client/01-why-http/get-issue.go
@@ -4,18 +4,18 @@ import (
 	"fmt"
 	"log"
 
-	"01-why-http/localpackage/http"
+	"01-why-http/localpackage/myhttp"
 )
 
 const issueURL = "https://api.boot.dev/v1/courses_rest_api/learn-http/issues"
 
 func main() {
-	issues, err := http.GetIssueData(issueURL)
+	issues, err := myhttp.GetIssues(issueURL)
 	if err != nil {
 		log.Fatalf("Error fetching issue data: %v", err)
 	}
 
-	prettyRes, err := http.PrettyPrint(string(issues))
+	prettyRes, err := myhttp.PrettyPrint(string(issues))
 	if err != nil {
 		log.Fatalf("Error pretty printing issue data: %v", err)
 	}
--- golang/boot-go-http-client/01-why-http/get-user.go
+++ golang/boot-go-http-client/01-why-http/get-user.go
@@ -1,50 +1,18 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
-	"net/http"
+
+	"01-why-http/localpackage/myhttp"
 )
 
 const usersUrl = "https://api.boot.dev/v1/courses_rest_api/learn-http/users"
 
 func main() {
-	users, err := getUsers(usersUrl)
+	users, err := myhttp.GetUsers(usersUrl)
 	if err != nil {
 		log.Fatalf("Error fetching users: %v", err)
 	}
 	fmt.Printf("%+v\n", users)
-}
-
-type User struct {
-	Id         string     `json:"id"`
-	Experience int        `json:"experience"`
-	Remote     bool       `json:"remote"`
-	Role       string     `json:"role"`
-	UserDetail UserDetail `json:"user"`
-}
-
-type UserDetail struct {
-	Name     string `json:"name"`
-	Location string `json:"location"`
-	Age      int    `json:"age"`
-}
-
-func getUsers(url string) ([]User, error) {
-	userResp, err := http.Get(url)
-	if err != nil {
-		return nil, fmt.Errorf("error fetching users: %w", err)
-	}
-
-	defer userResp.Body.Close()
-
-	var users []User
-	err = json.NewDecoder(userResp.Body).Decode(&users)
-
-	if err != nil {
-		return nil, fmt.Errorf("error reading users: %w", err)
-	}
-
-	return users, nil
 }
