5b600bc6608cc68f501ed4634ef86660c52f0e30
deadBlueShark <xspacechinguyen@gmail.com>
2025-03-07 17:58:46 +0700

    Marshal generic json(generic value)

---
+++ golang/boot-go-http-client/chap02-json/marshal_generic.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+func main() {
+	// any is an alias for interface{}
+	var data map[string]interface{}
+
+	jsonString := `{"name": "Alice", "age": 30, "address": {"city": "Wonderland"}}`
+	dataInBytes := []byte(jsonString)
+	err := json.Unmarshal(dataInBytes, &data)
+	if err != nil {
+		log.Fatal("unmarshalling error: ", err)
+	}
+
+	fmt.Println(data)            // map[address:map[city:Wonderland] age:30 name:Alice]
+	fmt.Println(data["name"])    // Alice
+	fmt.Println(data["address"]) // map[city:Wonderland]
+
+	// Type assertion
+	addressMap := data["address"].(map[string]any)
+	fmt.Println(addressMap["city"]) // Wonderland
+}
