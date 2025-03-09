2560e146202ad720945739543f0826b30b872cd2
deadBlueShark <xspacechinguyen@gmail.com>
2025-03-09 22:13:33 +0700

    Use url.JoinPath to handle request url/params

--- golang/boot-go-http-client/chap02-json/request.go
+++ golang/boot-go-http-client/chap02-json/request.go
@@ -3,12 +3,35 @@ package main
 import (
 	"chap02-json/resources"
 	"fmt"
+	"net/url"
 )
 
 const baseURL = "https://api.boot.dev"
 
 func main() {
-	issues, err := resources.GetResources(baseURL + "/v1/courses_rest_api/learn-http/issues?limit=1")
+	// Parse the base URL
+	base, err := url.Parse(baseURL)
+	if err != nil {
+		fmt.Println("Error parsing base URL:", err)
+		return
+	}
+
+	// Join the base URL with the path
+	path, err := url.JoinPath(base.String(), "/v1/courses_rest_api/learn-http/issues")
+	if err != nil {
+		fmt.Println("Error joining URL path:", err)
+		return
+	}
+
+	// Create query parameters
+	params := url.Values{}
+	params.Add("limit", "1")
+
+	// Construct the final URL
+	finalURL := fmt.Sprintf("%s?%s", path, params.Encode())
+
+	// Get and log the resources
+	issues, err := resources.GetResources(finalURL)
 	if err != nil {
 		fmt.Println("Error getting issues:", err)
 		return
